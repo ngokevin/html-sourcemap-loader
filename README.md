d08471ea0aa55dc008e9c4a23412e39c5826dae9
Kevin Ngo <me@ngokevin.com>
2019-03-06 22:48:39 -0800

    change to use cheerio

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 ## html-sourcemap-loader
 
 Webpack loader that creates a source map for HTML via data attributes and a
-JSON file.
+JSON file that supports generated HTML.
 
 Useful for web development editor tools to help modify source code live.
+
+In v1.2.0, the sourcemaps used to contain line and column numbers. Now it
+provides the order defined of the HTML element, done via recursive walking with
+the [cheerio](https://github.com/cheeriojs/cheerio) HTML parser. The previous
+implementation was more agnostic, but I changed it to be based on an HTML
+parser for ease of my use case.
 
 ### Usage
 
@@ -18,10 +24,9 @@ npm install --save html-sourcemap-loader
 
 #### Configuration
 
-| Option     | Description                                                                    | Default                          |
-|------------|--------------------------------------------------------------------------------|----------------------------------|
-| outputPath | Where to output `.html.map.json` sourcemap file.                               | Same path as required HTML file. |
-| tag        | Regex to filter what elements to attach `data-sm` sourcemap data attribute to. | *                                |
+| Option     | Description                                      | Default                          |
+|------------|--------------------------------------------------|----------------------------------|
+| outputPath | Where to output `.html.map.json` sourcemap file. | Same path as required HTML file. |
 
 ```js
 module.exports = {
@@ -35,8 +40,7 @@ module.exports = {
           {
             loader: 'html-sourcemap-loader',
             options: {
-              outputPath: __dirname,
-              tag: /.*?/
+              outputPath: __dirname
             }
           }
         ]
@@ -57,8 +61,10 @@ require('./index.html');
 Sourcemap data attributes will be injected into elements:
 
 ```html
-<h1 id="foo" data-sm="0"></h1>
-<h2 id="bar" data-sm="1"></h2>
+<h1 id="foo" data-sm="0">
+  <span id="bar" data-sm="1"></span>
+</h1>
+<h2 id="bar" data-sm="2"></h2>
 ```
 
 Then a sourcemap JSON file will be output on change (`.html.map.json`)
@@ -66,20 +72,22 @@ depending on `outputPath`:
 
 ```js
 {
+  0: {
+    file: '/path/to/index.html',
+    index: 0
+  },
   1: {
     file: '/path/to/index.html',
-    line: 0,
-    column: 0,
-    elementIndex: 0
-  },
+    index: 1
+  }
   2: {
     file: '/path/to/index.html',
-    line: 1,
-    column: 0,
-    elementIndex: 0
+    index: 2
   }
 }
 ```
+
+The index is essentially the order defined in the HTML file.
 
 This JSON can be used to reference live HTML elements to their position in the
 source code.
--- index.js
+++ index.js
@@ -1,80 +1,60 @@
+const cheerio = require('cheerio');
 const fs = require('fs');
 const getOptions = require('loader-utils').getOptions;
 const path = require('path');
 
-const endRequireRe = /<!-- <\/require> -->/g;
-const requireRe = /<!-- <require path="(.*?)"> -->/g;
-const sourcemapIdRe = /data-sm="(.*?)"/;
-const tagRe = /<\w[^'">]*(("[^"]*"|'[^']*')[^'">]*)*>/g
+const endRequireRe = /<\/require>/;
+const requireRe = /<require path="(.*?)">/;
 
 module.exports = function (source) {
   // Options.
   let options = getOptions(this);
   options = options || {};
   const outPath = options.outputPath || path.dirname(this.resourcePath);
-  const tagWhitelist = options.tag || null;
 
-  const sourcemap = {};
+  const $ = cheerio.load(source);
+
+  const fileStack = [{file: this.resourcePath, fileId: 0, index: 0}];
+  const sourcemap = {files: [this.resourcePath]};
+  const files = sourcemap.files;
   let sourcemapId = 0;
 
-  // Walk though file and see what file where this tag is defined.
-  let fileStack = [{file: this.resourcePath, lineNumber: 0, index: 0}];
-  const lines = source.split('\n');
-  const newLines = [];
-  lines.forEach(line => {
-    const originalLine = line;
-    const requireMatch = requireRe.exec(line);
+  addChildrenSourcemap($('body')[0].children);
 
-    // Inside another file.
-    if (requireMatch) {
-      fileStack.unshift({file: requireMatch[1], lineNumber: 0, index: 0});
-      return;
-    }
-
-    // Back up a file.
-    if (line.match(endRequireRe)) {
-      fileStack.shift();
-      return;
-    }
-
-    // Walk through tags of the line.
-    let match;
-    while (match = tagRe.exec(line)) {
-      if (match[0].indexOf('<require') !== -1 ||
-          match[0].indexOf('data-sm=') !== -1) { continue; }
-
-      // Attach sourcemap ID.
-      // Tag regex option to filter what to attach sourcemaps for.
-      if (!tagWhitelist || match[0].match(tagWhitelist)) {
-        let id = sourcemapId++;
-
-        // Use integer to give potential hot reloaders less work.
-        const updatedTag = match[0].replace(/>$/, ` data-sm="${id}">`);
-
-        sourcemap[id] = {
-          file: fileStack[0].file,
-          line: fileStack[0].lineNumber,
-          column: line.indexOf(match[0]),
-          length: match[0].length,
-          index: fileStack[0].index + match.index
-        };
-
-        line = line.replace(match[0], updatedTag);
+  function addChildrenSourcemap (children) {
+    children.forEach(el => {
+      if (el.type === 'comment') {
+        const requireMatch = requireRe.exec(el.data);
+        if (requireMatch) {
+          const filename = requireMatch[1];
+          if (files.indexOf(filename) === -1) { files.push(filename); }
+          fileStack.unshift({
+            file: filename,
+            fileId: files.indexOf(filename),
+            index: 0
+          });
+        }
+        const endRequireMatch = endRequireRe.exec(el.data);
+        if (endRequireMatch) { fileStack.shift(); }
+        return;
       }
-    }
 
-    fileStack[0].index += originalLine.length;
-    fileStack[0].lineNumber++;
-    newLines.push(line);
-  });
+      if (el.type !== 'tag') { return; }
+
+      const id = sourcemapId++;
+      $(el).attr('data-sm', id);
+      sourcemap[id] = {
+        file: fileStack[0].fileId,
+        index: fileStack[0].index++
+      };
+
+      if (el.children) { addChildrenSourcemap(el.children); }
+    });
+  }
 
   fs.writeFileSync(path.resolve(outPath, '.html.map.json'),
                    JSON.stringify(sourcemap));
-  source = source.replace(requireRe, '');
-  source = source.replace(endRequireRe, '');
-  return newLines.join('\n');
+  return $.html()
+    .replace(requireRe, '')
+    .replace(endRequireRe, '');
 };
-
-function randStr () {
-  return Math.random().toString(36).substring(7);
-}
